646d76168b76bfd2b9a96c25ef499e042aa01ef3
mxyD <2390140586@qq.com>
2019-11-05 14:17:40 +0800

    第二次作业

---
+++ build/precache-manifest.6ca677d1efd1e0fcefe65b4021af3cac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60d2854a3195cf7e442e9da1f7cafac0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "99a86f57cb423b7e6341",
+    "url": "/static/css/2.0b726e0e.chunk.css"
+  },
+  {
+    "revision": "d2edef3496ba9eaa32fc",
+    "url": "/static/css/main.d2d17362.chunk.css"
+  },
+  {
+    "revision": "99a86f57cb423b7e6341",
+    "url": "/static/js/2.81cb4ff0.chunk.js"
+  },
+  {
+    "revision": "d2edef3496ba9eaa32fc",
+    "url": "/static/js/main.6bc066df.chunk.js"
+  },
+  {
+    "revision": "628ea25a63cbc5b89a27",
+    "url": "/static/js/runtime-main.9f147cbc.js"
+  }
+]);
